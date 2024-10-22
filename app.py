3bb7c4b10074e81b6e755f5d2dc088d5370705af
KardosLevente <kardos.levente@diak.szbi-pg.hu>
2024-10-22 09:40:19 +0200

    Add signup_form html file for user registration

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+products = []
 
-@app.route('/',methods=['GET', 'POST'])
+@app.route('/', methods=["GET"])
 def index():
-    name = None
-    if request.method == 'POST':
-        name = request.form.get('product_name')
-    return render_template('index.html', name=name)
+    return render_template('index.html')
+
+
+@app.route('/submit_products', methods=["POST"])
+def submit_products():
+    name = request.form.get("product_name")
+    products.append(name)
+    return redirect(url_for('index'))
+
+@app.route('/products')
+def get_products():
+    return render_template('products.html', products_=products)
 
 if __name__ == "__main__":
     app.run(debug=True)
